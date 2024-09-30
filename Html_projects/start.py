41dd1ffcaad70260a3efe4565b7dd821ca1c7f55
xSSanDev <xssanass@gmail.com>
2024-09-30 18:31:54 +0000

    .

---
+++ Html_projects/start.py
@@ -0,0 +1,13 @@
+CTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Basic HTML Page</title>
+            </head>
+            <body>
+                <h1>Welcome to My Basic HTML Page</h1>
+                    <p>This is a paragraph of text on my basic HTML page.</p>
+                    </body>
+                    </html>
+
